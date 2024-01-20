67abfc931592e28e15aab3a77db3d6aac29da897
Natanael <ngsneto@gmail.com>
2024-01-20 13:59:03 -0300

    add

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const cypress = require("cypress");
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    
+      // implement node event listeners here
+    },
+  },
+});
+
+
+// arquivo de configuração do cypress
